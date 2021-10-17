179d9cf4f3abb2d74d732800cad9fedb3bc82c60
JoaquinS <joaquinsomoza6@gmail.com>
2021-10-16 21:18:20 -0300

    Arreglamos el footer y toda la navegacion de links

--- app.js
+++ app.js
@@ -13,20 +13,20 @@ app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/index.html'));
 });
 
-app.get('/signUp', ( req, res) => {
-    res.sendFile(path.resolve(__dirname,'./views/register.html'));
+app.get('/signUp', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/register.html'));
 });
 
-app.get('/login', ( req, res) => {
-    res.sendFile(path.resolve(__dirname,'./views/login.html'));
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/login.html'));
 });
 
-app.get('/productDetail', ( req, res) => {
-    res.sendFile(path.resolve(__dirname,'./views/productDetail.html'))
+app.get('/productDetail', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/productDetail.html'))
 });
 
-app.get('/productCart', ( req, res) => {
-    res.sendFile(path.resolve(__dirname,'./views/productCart.html'))
+app.get('/productCart', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/productCart.html'))
 });
 
 app.listen(PUERTO, () => {
