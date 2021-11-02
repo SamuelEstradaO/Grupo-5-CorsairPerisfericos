4a97174e8c0ba4292f010b3f0f787e04063ccc4e
SamEstO <sy.estrada.o@gmail.com>
2021-11-01 19:37:52 -0600

    parametrizacion de edit, separacion de create y edit y correcion de rutas

--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -2,8 +2,9 @@ const express = require("express");
 const router = express.Router();
 const productsController = require("../controllers/productsController");
 
-router.get("/productDetail", productsController.product);
-router.get("/productCart", productsController.cart);
-router.get("/productAdmin", productsController.admin);
+router.get("/detail", productsController.product);
+router.get("/cart", productsController.cart);
+router.get("/create", productsController.create);
+router.get("/edit/:id", productsController.edit);
 
 module.exports = router;
