f8eca9a2bef50d556b98ffb218bff04af9bf085b
Mario Martinez <mariomtz091@gmail.com>
2022-02-20 03:05:34 -0300

    Carrito v1

--- public/js/agregarProducto.js
+++ public/js/agregarProducto.js
@@ -1,39 +1,24 @@
 window.onload = function () {
-    const btnAgregar = document.getElementsByClassName("btn__agregar");
-    for (let i = 0; i < btnAgregar.length; i++) {
-        btnAgregar[i].addEventListener("click", function () {
-            let producto = btnAgregar[i].parentNode.parentNode;
-            console.log(producto.id)
-            let cart = JSON.parse(localStorage.getItem("cart"));
-            if (cart == null) {
-                cart = [];
-            }
-            cart.push(producto.id);
-            localStorage.setItem("cart", JSON.stringify(cart));
-            alert("Producto agregado al carrito");
-        });
-
-    }
-
-
-    /*  btnAgregar.(btn => {
-         btn.addEventListener("click", function () {
-             let producto = btn.parentNode.parentNode;
-             console.log(producto) */
-    /* let productoAgregado = {
-        nombre: producto.querySelector(".main__article--description h4").innerText,
-        precio: producto.querySelector(".main__article--option p:nth-child(2)").innerText,
-        cantidad: producto.querySelector(".main__article--option select").value,
-        total: producto.querySelector(".main__article--option p:nth-child(3)").innerText
-    }
-    let cart = JSON.parse(localStorage.getItem("cart"));
-    if (cart == null) {
+  const btnAgregar = document.getElementsByClassName("btn__agregar");
+  for (let i = 0; i < btnAgregar.length; i++) {
+    btnAgregar[i].addEventListener("click", function () {
+      let producto = btnAgregar[i].parentNode.parentNode;
+      let cart = JSON.parse(localStorage.getItem("cart"));
+      if (cart == null) {
         cart = [];
-    }
-    cart.push(productoAgregado);
-    localStorage.setItem("cart", JSON.stringify(cart));
-    alert("Producto agregado al carrito"); */
+      }
 
+      const index = cart.findIndex((product) => product.id == producto.id);
 
-    /* console.log(btnAgregar) */
-}
+      if (index != -1) {
+        cart[index].cantidad++;
+      } else {
+        cart.push({ id: producto.id, cantidad: 1 });
+      }
+      console.log(cart);
+
+      localStorage.setItem("cart", JSON.stringify(cart));
+      alert("Producto agregado al carrito");
+    });
+  }
+};
--- public/js/cart.js
+++ public/js/cart.js
@@ -1,23 +1,38 @@
-window.onload = function () {
-    let cartBox = document.getElementsByClassName("box-cart");
+window.onload = function async() {
+  let cartBox = document.getElementsByClassName("box-cart");
 
-    //Local Storage
-    function obtenerProductos() {
-        let cart = JSON.parse(localStorage.getItem("cart"));
-        if (cart == null) {
-            cart = [];
-        }
-        return cart;
+  //Local Storage
+  function obtenerProductos() {
+    let cart = JSON.parse(localStorage.getItem("cart"));
+    if (cart == null) {
+      cart = [];
     }
-    let productos = obtenerProductos();
-    productos.map(producto => {
-        cartBox.innerHTML += `<div class="main__article--img">
+    return cart;
+  }
+  let productos = obtenerProductos();
+
+  const getProduct = async (productos) => {
+    const carrito = [];
+    for await (const producto of productos) {
+      let product = await fetch(`/api/products/${producto.id}`);
+      product = await product.json();
+      carrito.push(product);
+    }
+    // console.log(product);
+    return carrito;
+  };
+
+  const waiteado = await getProduct(productos);
+  console.log(waiteado);
+
+  for (const producto of productos) {
+    cartBox.innerHTML += `<div class="main__article--img">
         <img src="\images\#" alt="#" />
       </div>
       <div class="main__article--description">
-        <h4>#</h4>
+        <h4>${product.titulo}</h4>
         <p>
-        #
+        ${product.descripcion}
         </p>
         <div>
           <ul class="main__article--list">
@@ -38,12 +53,42 @@ window.onload = function () {
         </select>
         <p>#</p>
         <p>#</p>
-      </div>`
-    });
-    localStorage.getItem("cart");
+      </div>`;
+  }
+  // productos.map((producto) => {
+  //   const product = getProduct(producto);
+  //   cartBox.innerHTML += `<div class="main__article--img">
+  //       <img src="\images\#" alt="#" />
+  //     </div>
+  //     <div class="main__article--description">
+  //       <h4>${product.titulo}</h4>
+  //       <p>
+  //       ${product.descripcion}
+  //       </p>
+  //       <div>
+  //         <ul class="main__article--list">
+  //           <li><a href="#">Borrar</a></li>
+  //           <li><a href="#">Ir a local</a></li>
+  //           <li><a href="#">Guardar</a></li>
+  //         </ul>
+  //       </div>
+  //     </div>
+  //     <div class="main__article--option">
+  //       <select name="" id="">
+  //         <option value="" disabled selected>Cantidad</option>
+  //         <option value="1">1</option>
+  //         <option value="2">2</option>
+  //         <option value="3">3</option>
+  //         <option value="4">4</option>
+  //         <option value="5">5</option>
+  //       </select>
+  //       <p>#</p>
+  //       <p>#</p>
+  //     </div>`;
+  // });
+  // localStorage.getItem("cart");
 
-
-    localStorage.setItem('key', 'value');
-    localStorage.getItem('key');
-    localStorage.removeItem('key');
-}
+  // localStorage.setItem("key", "value");
+  // localStorage.getItem("key");
+  // localStorage.removeItem("key");
+};
